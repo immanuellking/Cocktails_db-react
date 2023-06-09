aa50a3533fb3ad80abf5f0741d7f2024ecf79ef3
immanuellking <emmaebubeegbosi@gmail.com>
2023-06-09 08:30:33 +0100

    second commit

--- src/context.js
+++ src/context.js
@@ -5,12 +5,14 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
+
+
   return (
     <AppContext.Provider
-      value={{ loading, searchTerm, cocktails, setSearchTerm }}
+      value={{ loading, cocktails, setSearchTerm }}
     >
       {children}
     </AppContext.Provider>
