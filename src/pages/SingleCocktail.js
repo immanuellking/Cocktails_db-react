5f06af91b6e2f9d10a9308fff638f2879ca1d7a1
immanuellking <emmaebubeegbosi@gmail.com>
2023-06-12 12:49:17 +0100

    final commit

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -70,8 +70,42 @@ const SingleCocktail = () => {
   const { name, image, info, category, glass, instructions, ingredients } =
     cocktail;
   return (
-    <section>
+    <section className="section cocktail-section">
+      <Link to="/" className="btn btn-primary">
+        back home
+      </Link>
       <h2 className="section-title">{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name :</span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">category :</span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">info :</span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">glass :</span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instructions :</span>
+            {instructions}
+          </p>
+          <p>
+            <span className="drink-data">ingredients :</span>
+            {ingredients.map((item, index) => {
+              return item ? <span key={index}>{item}</span> : null;
+            })}
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
